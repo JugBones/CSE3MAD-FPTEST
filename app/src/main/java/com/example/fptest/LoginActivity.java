9a40e1a31c2c0166ae2100ac899890cf256c6f9b
Joshua Alexander <91466077+joshualxndrs@users.noreply.github.com>
2024-05-08 11:25:56 +0700

    update

--- app/src/main/java/com/example/fptest/LoginActivity.java
+++ app/src/main/java/com/example/fptest/LoginActivity.java
@@ -3,7 +3,6 @@ package com.example.fptest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -32,44 +31,31 @@ public class LoginActivity extends AppCompatActivity {
         forgotPassword = findViewById(R.id.forgot_password);
         auth = FirebaseAuth.getInstance();
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = loginEmail.getText().toString();
-                String pass = loginPassword.getText().toString();
-                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    if (!pass.isEmpty()) {
-                        auth.signInWithEmailAndPassword(email, pass)
-                                .addOnSuccessListener(authResult -> {
-                                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                                    startActivity(new Intent(LoginActivity.this, HomePage.class));
-                                    finish();
-                                }).addOnFailureListener(e -> {
-                                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
-                                });
-                    } else {
-                        loginPassword.setError("Empty fields are not allowed");
-                    }
-                } else if (email.isEmpty()) {
-                    loginEmail.setError("Empty fields are not allowed");
+        loginButton.setOnClickListener(v -> {
+            String email = loginEmail.getText().toString();
+            String pass = loginPassword.getText().toString();
+            if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                if (!pass.isEmpty()) {
+                    auth.signInWithEmailAndPassword(email, pass)
+                            .addOnSuccessListener(authResult -> {
+                                Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this, HomePage.class));
+                                finish();
+                            }).addOnFailureListener(e -> Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show());
                 } else {
-                    loginEmail.setError("Please enter correct email");
+                    loginPassword.setError("Empty fields are not allowed");
                 }
+            } else if (email.isEmpty()) {
+                loginEmail.setError("Empty fields are not allowed");
+            } else {
+                loginEmail.setError("Please enter correct email");
             }
         });
 
-        signupRedirectText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
-            }
-        });
+        signupRedirectText.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, SignupActivity.class)));
 
-        forgotPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Forgot password functionality
-            }
+        forgotPassword.setOnClickListener(view -> {
+            // Forgot password functionality
         });
     }
 }
