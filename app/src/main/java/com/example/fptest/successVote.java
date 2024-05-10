a4dfb93ca1ee619d6df0fccdffb0237c6741f10b
JugBones <christopher.tjiandra@binus.ac.id>
2024-05-10 15:37:42 +0700

    update functionality

--- app/src/main/java/com/example/fptest/successVote.java
+++ app/src/main/java/com/example/fptest/successVote.java
@@ -3,6 +3,8 @@ package com.example.fptest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.Button;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +16,9 @@ public class successVote extends AppCompatActivity {
         setContentView(R.layout.activity_success_vote);
 
         ImageButton Home_Btn = findViewById(R.id.home_btn);
+        Button Poll_Btn = findViewById(R.id.goto_poll_btn);
 
         Home_Btn.setOnClickListener(v -> startActivity(new Intent(successVote.this, HomePage.class)));
+        Poll_Btn.setOnClickListener(v -> startActivity(new Intent(successVote.this, LivePolling.class)));
     }
 }
