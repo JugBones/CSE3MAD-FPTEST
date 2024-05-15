e630c69e41b2db81f626de613b4810acf2425b45
JugBones <christopher.tjiandra@binus.ac.id>
2024-05-15 22:48:07 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,19 +31,27 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:compose-theme-adapter:1.2.1")
     implementation("com.hanks:passcodeview:0.1.2")
-    implementation("com.google.firebase:firebase-inappmessaging:21.0.0")
-    implementation("com.google.firebase:firebase-storage:21.0.0")
-    implementation("com.google.firebase:firebase-auth:23.0.0")
+
+    // Firebase BOM
+    implementation(platform("com.google.firebase:firebase-bom:32.0.0"))
+
+    // Firebase dependencies
+    implementation("com.google.firebase:firebase-inappmessaging")
+    implementation("com.google.firebase:firebase-storage")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-database")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("com.android.support.test:runner:1.0.2")
+
     implementation ("androidx.multidex:multidex:2.0.1")
     implementation("com.google.zxing:core:3.4.1")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
@@ -51,7 +59,4 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
-    implementation("com.google.firebase:firebase-database:20.3.0")
-    implementation ("com.google.firebase:firebase-firestore:24.0.1")
-
 }
