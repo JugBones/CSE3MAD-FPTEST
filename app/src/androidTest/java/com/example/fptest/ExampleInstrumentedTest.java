a3d140adb4769d586fd7717921f9cdb3349fc369
JugBones <christopher.tjiandra@binus.ac.id>
2024-06-01 01:04:50 +0700

    update testing

--- app/src/androidTest/java/com/example/fptest/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/fptest/ExampleInstrumentedTest.java
@@ -1,14 +1,17 @@
 package com.example.fptest;
 
-import android.content.Context;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import android.content.Context;
+import android.widget.ImageButton;
+
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -23,4 +26,28 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.fptest", appContext.getPackageName());
     }
+
+    @Test
+    public void testAppResources() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        String appName = appContext.getString(R.string.app_name);
+        assertEquals("Vote.Inc", appName);
+    }
+
+    @Test
+    public void testButtonsPresence() {
+        try (ActivityScenario<HomePage> scenario = ActivityScenario.launch(HomePage.class)) {
+            scenario.onActivity(activity -> {
+                ImageButton profileButton = activity.findViewById(R.id.prof_btn);
+                ImageButton scanButton = activity.findViewById(R.id.scan_btn);
+                ImageButton livepolButton = activity.findViewById(R.id.livepol_btn);
+
+                assertNotNull(profileButton);
+                assertNotNull(scanButton);
+                assertNotNull(livepolButton);
+            });
+        }
+    }
+
+
 }
